f7867d1cefd15677cad63808c122a0de1106f9f9
Arseny Chebyshev <96249787+arseny-chebyshev@users.noreply.github.com>
2023-04-27 20:43:08 +0600

    Добавил хендлер на команду /end

--- bot.py
+++ bot.py
@@ -18,7 +18,11 @@ dp = Dispatcher(bot)
 async def echo(message: types.Message):
     await message.reply('Start')
 
-
+@dp.message_handler(commands=['end'])
+async def echo(message: types.Message):
+    await message.reply('end')
+    
+    
 @dp.message_handler(commands=['help'])
 async def echo(message: types.Message):
     await message.reply('Вы обратились к справке бота')
